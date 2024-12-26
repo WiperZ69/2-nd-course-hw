3f00fc00b9c918d6996a6556aebbb24e14f231ee
WiperZ <yura.4etin.03@gmail.com>
2024-12-26 18:13:42 +0800

    do 7hw, add 3 and 4 game

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Кнопка назад
+let goBack = function (){
+    window.history.back();
+};
+
 // 1
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -103,4 +108,66 @@ function gameTwo() {
         }
     }
 };
+
+// 3
+function generateVersa(text){
+    return text.split('').reverse().join('');
+}
+function gameThree() {
+    let userInput;
+    let i = 0;
+    while (true) {
+        userInput = prompt(`Введи текст, чтобы я его перевернул`);
+        if (userInput === null) {
+            alert("Игра окончена. Спасибо за игру!");
+            break;
+        }
+        let userString = String(userInput);
+        alert(generateVersa(userString));
+        break;
+    }
+};
+
+// 4
+const quiz = [
+    {
+        question: "Какого цвета небо?",
+        options: [" 1. Красного ", "2. Синего ", "3. Зеленого"],
+        correctAnswer: 2
+    },
+    {
+        question: "Сколько дней в неделе?",
+        options: [" 1. Шесть ", "2. Семь ", "3. Восемь"],
+        correctAnswer: 2
+    },
+    {
+        question: "Сколько у человека пальцев на одной руке?",
+        options: [" 1. Четыре ", "2. Пять ", "3. Шесть"],
+        correctAnswer: 2
+    }
+];
+function gameFour(){
+    let score = 0;
+    let err = 0;
+    for (let i = 0; i < quiz.length; i++) {
+        const currentQuestion = quiz[i];
+        let questionText = currentQuestion.question;
+        for (let j = 0; j < currentQuestion.options.length; j++) {
+            questionText += currentQuestion.options[j];
+        }
+        const userAnswer = prompt(questionText);
+        if (userAnswer === null) {
+            alert("Игра окончена. Спасибо за игру!");
+            err++;
+            break;
+        }
+        if (parseInt(userAnswer) === currentQuestion.correctAnswer) {
+            score++;
+        }
+    }
+    if (err != 1) {
+        
+        alert(`Вы ответили правильно на ${score} из ${quiz.length} вопросов.`);
+    }
+}
 
